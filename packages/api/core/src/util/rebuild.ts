b205c2fa05b3184c8e0026531030604fb90b34d8
Phablulo <phablulo@live.com>
2020-03-22 20:35:24 -0300

    testando

--- packages/api/core/src/util/rebuild.ts
+++ packages/api/core/src/util/rebuild.ts
@@ -1,35 +1,21 @@
 import { asyncOra } from '@electron-forge/async-ora';
 import { ForgePlatform, ForgeArch } from '@electron-forge/shared-types';
+import { exec } from 'child_process';
 
-import rebuild from 'electron-rebuild';
-import { RebuildOptions } from 'electron-rebuild/lib/src/rebuild';
+// import rebuild from 'electron-rebuild';
+// import { RebuildOptions } from 'electron-rebuild/lib/src/rebuild';
 
 export default async (
   buildPath: string,
   electronVersion: string,
   platform: ForgePlatform,
   arch: ForgeArch,
-  config: Partial<RebuildOptions> = {},
 ) => {
-  await asyncOra('Preparing native dependencies', async (rebuildSpinner) => {
-    const rebuilder = rebuild({
-      ...config,
-      buildPath,
-      electronVersion,
-      arch,
+  await asyncOra('Preparing native dependencies', () => new Promise((resolve, reject) => {
+    const buildString = `npm rebuild --runtime=electron --target=${electronVersion} --arch=${arch} --rebuild --disturl=https://atom.io/download/electron --build_from_source=true`;
+    return exec(buildString, (error, stdout, stderr) => {
+      if (error) return reject(new Error(`Erro no rebuild: ${error}: ${stderr}`));
+      return resolve(stdout);
     });
-    const { lifecycle } = rebuilder;
-
-    let found = 0;
-    let done = 0;
-
-    const redraw = () => {
-      rebuildSpinner.text = `Preparing native dependencies: ${done} / ${found}`;
-    };
-
-    lifecycle.on('module-found', () => { found += 1; redraw(); });
-    lifecycle.on('module-done', () => { done += 1; redraw(); });
-
-    await rebuilder;
-  });
+  }));
 };
